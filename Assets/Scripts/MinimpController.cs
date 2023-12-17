588b14a7639ffeee1eeed37e9589dd474debabdd
Ahn Sumin <asm9586@gmail.com>
2023-12-17 17:50:11 +0900

    미니맵 수정3

--- Assets/Scripts/MinimpController.cs
+++ Assets/Scripts/MinimpController.cs
@@ -3,16 +3,23 @@ using UnityEngine;
 
 public class MinimpController : MonoBehaviour
 {
-    public Transform playerTransform; // 실제 플레이어 Transform
-    public float iconScale = 1.5f; // 아이콘의 크기 배율
-    void Start()
+    public GameObject mapPanel;
+    private void Start()
     {
-        // 아이콘의 크기를 설정된 배율로 조정
-        transform.localScale = new Vector3(iconScale, iconScale, iconScale);
+        //맵 비활성화
+        mapPanel.SetActive(false);
     }
     void Update()
     {
-        // 아이콘의 위치를 플레이어의 현재 위치로 업데이트
-        transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y, transform.position.z);
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            Debug.Log("M key pressed");
+            mapPanel.SetActive(!mapPanel.activeSelf);
+        }
+        if (Input.anyKeyDown)
+        {
+            Debug.Log("Key pressed");
+        }
+
     }
 }
