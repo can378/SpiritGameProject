064b76280e6d1887f2efa05d723895ab7d0518d5
can378 <yunji378mm@naver.com>
2024-02-16 01:46:54 +0900

    mission room
    머리가 안굴러가서 멈춰요

--- Assets/Scripts/Map Gen/Mission.cs
+++ Assets/Scripts/Map Gen/Mission.cs
@@ -2,19 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum MissionType {MiniBoss, TimeAttack}
+public enum MissionType {KillAll, NoHurt, MiniBoss, TimeAttack,Jump,Lava,Dream}
 
 public class Mission : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public MissionType type;
+
+    private void Update()
+    {
+       
+    }
+
+    //starts when the map is generated
+    public IEnumerator CheckMissionEnd() 
+    {
+
+        switch (type)
+        {
+            case MissionType.KillAll:
+                break;
+            case MissionType.NoHurt:
+                break;
+            case MissionType.MiniBoss:
+                break;
+            case MissionType.TimeAttack:
+                break;
+            case MissionType.Jump:
+                break;
+            case MissionType.Lava:
+                break;
+            case MissionType.Dream:
+                break;
+            default:
+                break;
+        }
+        yield return null;
+        StartCoroutine(CheckMissionEnd());
+    }
+
+    private void CheckKillAll()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
