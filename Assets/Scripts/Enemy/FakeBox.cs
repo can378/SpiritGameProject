fad9d60daa3a428a89006a806526623252eb87bd
can378 <63956527+can378@users.noreply.github.com>
2024-03-30 22:38:53 +0900

    enemy-공통적
    귀구 뺴고 다함

---
+++ Assets/Scripts/Enemy/FakeBox.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FakeBox : EnemyBasic
+{
+    public GameObject mouth;
+    void Start()
+    {
+        StartCoroutine(fakeBox());
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(fakeBox());
+    }
+
+    private void OnDisable()
+    {
+        StopCoroutine(fakeBox());
+    }
+    IEnumerator fakeBox() 
+    {
+        targetDis = Vector2.Distance(transform.position, enemyTarget.position);
+        print("fakeBox1");
+        
+        if (targetDis < 10f && Input.GetKeyDown(KeyCode.F))
+        {
+            print("fakeBox2");
+            mouth.SetActive(true);
+            yield return new WaitForSeconds(5f);
+            mouth.SetActive(false);
+        }
+        yield return new WaitForSeconds(0.01f);
+
+        StartCoroutine(fakeBox());
+        
+    }
+}
---
+++ Assets/Scripts/Enemy/InventoryThief.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryThief : EnemyBasic
+{
+    private bool isSteal=false;
+    
+    void Start()
+    {
+        StartCoroutine(stealItem());
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(stealItem());
+    }
+
+    private void OnDisable()
+    {
+        StopCoroutine(stealItem());
+    }
+
+    IEnumerator stealItem() 
+    {
+        if (isSteal==false)
+        {
+            Chase();
+        }
+        else 
+        {
+            //runaway
+
+            targetDirVec = (enemyTarget.position - transform.position).normalized;
+            targetDis = Vector2.Distance(transform.position, enemyTarget.position);
+            rigid.AddForce(-targetDirVec * GetComponent<EnemyStats>().defaultMoveSpeed*10);
+
+
+        }
+        yield return new WaitForSeconds(0.1f);
+        StartCoroutine(stealItem());
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            if (enemyTarget.GetComponent<Player>().playerItem != null)
+            {
+
+                Destroy(enemyTarget.GetComponent<Player>().playerItem);
+                enemyTarget.GetComponent<Player>().playerItem = null;
+                MapUIManager.instance.updateItemUI(null);
+                isSteal = true;
+            }
+            
+        }
+    }
+}
