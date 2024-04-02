1e5fe2cf0b7fe063c9786a5cb2745cbaa98bb2da
can378 <63956527+can378@users.noreply.github.com>
2024-04-02 16:40:11 +0900

    .
    .

--- Assets/Scripts/Enemy/FakeBox.cs
+++ Assets/Scripts/Enemy/FakeBox.cs
@@ -24,7 +24,7 @@ public class FakeBox : EnemyBasic
         targetDis = Vector2.Distance(transform.position, enemyTarget.position);
         print("fakeBox1");
         
-        if (targetDis < 10f && Input.GetKeyDown(KeyCode.F))
+        if (targetDis < 3f && Input.GetKeyDown(KeyCode.F))
         {
             print("fakeBox2");
             mouth.SetActive(true);
