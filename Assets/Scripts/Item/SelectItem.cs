665c77832948d5040c52a4d26723db35d2246c19
turtlehand <juny000903@gmail.com>
2024-06-26 15:12:30 +0900

    장비 수정 중

--- Assets/Scripts/Item/SelectItem.cs
+++ Assets/Scripts/Item/SelectItem.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using TMPro;
 
 public enum SelectItemClass { Equipments, Weapon, Consumable, Skill };
+public enum SelectItemRating { Temp, Normal, Rare, Epic, Legend }
 
 public class SelectItem : MonoBehaviour
 {
@@ -11,6 +12,6 @@ public class SelectItem : MonoBehaviour
     [field: SerializeField] public int selectItemID { get; private set; }
     [field: SerializeField] public string selectItemName { get; private set; }
     [field: SerializeField] public int price {get; private set;}
-    [field: SerializeField] public int selectItemRating { get; private set; }
+    [field: SerializeField] public SelectItemRating selectItemRating { get; private set; }
 }
 
