0abf83cb8b867743b10f1f4beece797175d8c2a1
turtlehand <juny000903@gmail.com>
2024-07-10 14:10:10 +0900

    적 체력바 완성

---
+++ Assets/Scripts/Enemy/EnemyUI/EnemyUI.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyUI : MonoBehaviour
+{
+    Stats stats;
+
+    public Slider Hpslider;
+
+    void Awake()
+    {
+        stats = GetComponent<Stats>();
+    }
+
+    void FixedUpdate()
+    {
+        UpdateHealthUI();
+    }
+
+    public void UpdateHealthUI()
+    {
+        float normalizedHealth = (stats.HP / stats.HPMax) * 100;
+
+        if( normalizedHealth >= 100f)
+        {
+            Hpslider.gameObject.SetActive(false);
+            return;
+        }
+
+        Hpslider.gameObject.SetActive(true);
+        Hpslider.value = normalizedHealth;
+    }
+}
