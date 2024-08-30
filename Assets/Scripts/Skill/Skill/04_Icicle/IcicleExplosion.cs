d4823e9b61dcaba6e0fc85ebe73bda9e650c141b
can378 <yunji378mm@naver.com>
2024-08-31 04:13:08 +0900

    skill 이미지 적용
    /.

---
+++ Assets/Scripts/Skill/Skill/04_Icicle/IcicleExplosion.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IcicleExplosion : HitDetection
+{
+
+    private bool isExploding=false;
+    public GameObject originalForm;
+    public GameObject explosinForm;
+
+
+    protected override void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy"||
+            collision.gameObject.tag == "Wall" || 
+            collision.gameObject.tag == "Door" || 
+            collision.gameObject.tag == "ShabbyWall" &&isExploding==false)
+        {
+
+            explosionSprite();
+        }
+
+        //base.OnTriggerEnter2D(collision);
+        
+       
+    }
+
+    public void explosionSprite() 
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        originalForm.SetActive(false);
+        explosinForm.SetActive(true);
+    }
+
+
+    
+
+}
