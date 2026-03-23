1ff2384fa734fd7ab1f357729e3c4cd2cc46eea9
can378 <yunji378mm@naver.com>
2026-03-23 22:06:18 +0900

    Feat: bossbaby audio scriptable object

---
+++ Assets/Scripts/Enemy/BossBabyAudio.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BossBabyAudio", menuName = "Game/Boss Baby Audio")]
+public class BossBabyAudio : EnemyAudio
+{
+    public AudioClip wallCrash;
+    public AudioClip boneCrash;
+    public AudioClip babyCry;
+    public AudioClip adultScream;
+}
