be9eef5792c3294956ee5ac85c114e7f435523ff
Kim JunHyeong <juny000903@gmail.com>
2026-06-06 14:43:47 +0900

    fix 도깨비 공격 소리 조정

--- Assets/Scripts/Enemy/Dokkebie/Dokkaebi.cs
+++ Assets/Scripts/Enemy/Dokkebie/Dokkaebi.cs
@@ -86,9 +86,11 @@ public class Dokkaebi : EnemyBasic
         // 애니메이션과 공격 판정 활성화
         enemyAnim.animator.SetBool("isAttack", true);
         hitEffects[0].gameObject.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+
         AudioManager.instance.PlaySFX(enemyAudio.attack);//공격 오디오
 
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.5f);
 
         // 애니메이션과 공격 판정 비활성화
         enemyAnim.animator.SetBool("isAttack", false);
